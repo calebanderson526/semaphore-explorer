5f5ef9b5724b0ff4a3fad8aee297860f4051d277
calebanderson526 <caleb.anderson526@gmail.com>
2023-04-15 02:31:14 -0400

    create explorer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
